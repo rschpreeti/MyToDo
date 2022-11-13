6ccbdb9d3d0c173996fe753e476c90b82b5a10ef
rschpreeti <112795364+rschpreeti@users.noreply.github.com>
2022-11-13 14:19:58 +0530

    Create AdvanceToDoApplication.java

---
+++ src/main/java/com/todo/AdvanceToDo/AdvanceToDoApplication.java
@@ -0,0 +1,13 @@
+package com.todo.AdvanceToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvanceToDoApplication.class, args);
+	}
+
+}
