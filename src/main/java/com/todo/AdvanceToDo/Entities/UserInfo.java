72d2421e0588b231cd66a3c55bc9f8b299071f03
rschpreeti <112795364+rschpreeti@users.noreply.github.com>
2022-11-13 14:13:30 +0530

    Create UserInfo.java

---
+++ src/main/java/com/todo/AdvanceToDo/Entities/UserInfo.java
@@ -0,0 +1,68 @@
+package com.todo.AdvanceToDo.Entities;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userId;
+    private String name;
+    private String mobNumber;
+    private String emailId;
+    private String password;
+
+    public UserInfo() {
+    }
+
+    public UserInfo(String name, String mobNumber, String emailId, String password) {
+        this.name = name;
+        this.mobNumber = mobNumber;
+        this.emailId = emailId;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobNumber() {
+        return mobNumber;
+    }
+
+    public void setMobNumber(String mobNumber) {
+        this.mobNumber = mobNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
